ad71fe3bf70fd338429fc20ecffc82a88ead0f28
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-04-28 18:52:34 +0200

    [add] page css + tilte

--- Pages/accueil.php
+++ Pages/accueil.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>accueil</title>
 </head>
 <body>
     
+    <h1><strong>Les amis des animaux</strong></h1>
+
 </body>
 </html>
