bf8beb839bba7f5d942eb6c258713cb99a6f8fd5
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-04-29 15:28:22 +0200

    [add] architecture + form inscription

--- Pages/accueil.php
+++ Pages/accueil.php
@@ -1,47 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    
-    <title>accueil</title>
+<?php include "header.php"; ?>
 
-    <!-- navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Accueil</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="animaux.php">Les animaux</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="inscription.php">Inscription</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="connexion.php">Connexion</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="deconnexion.php">Déconnexion</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="panier.php">Panier</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">autre rubrique</a>
-      </li>
-    </ul>
-  
-  </div>
-</nav>
-
-</head>
 <body>
     
     <!-- carousel -->
@@ -75,11 +33,6 @@
     
     <h1 class="center-text" style="font-size:60px"><u>Les amis des animaux</u></h1>
 
-
-    <!-- Logo/icones font awesome -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
-<footere>
-</html>
+
+<?php include "footer.php"; ?>
---
+++ Pages/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    
+    <title>accueil</title>
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Accueil</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="animaux.php">Les animaux</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="inscription.php">Inscription</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="connexion.php">Connexion</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="deconnexion.php">Déconnexion</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="panier.php">Panier</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">autre rubrique</a>
+      </li>
+    </ul>
+  
+  </div>
+  </nav>
+</head>
