3f4a26daeab2cfd29c1851d091e91a3ab30456c0
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-04-29 14:41:09 +0200

    [add] navbar

--- Pages/accueil.php
+++ Pages/accueil.php
@@ -6,6 +6,41 @@
     <link rel="stylesheet" href="CSS/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    
     <title>accueil</title>
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Accueil</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="animaux.php">Les animaux</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="inscription.php">Inscription</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="connexion.php">Connexion</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="deconnexion.php">Déconnexion</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="panier.php">Panier</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">autre rubrique</a>
+      </li>
+    </ul>
+  
+  </div>
+</nav>
+
 </head>
 <body>
     
@@ -19,13 +54,13 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-75" src="IMG/shiba.jpeg" alt="First slide">
+      <img class="d-block w-50" src="IMG/shiba.jpeg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-75" src="IMG/chat-sphynx.jpg" alt="Second slide">
+      <img class="d-block w-50" src="IMG/chat-sphynx.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-75" src="IMG/jackie.png" alt="Third slide">
+      <img class="d-block w-50" src="IMG/jackie.png" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
