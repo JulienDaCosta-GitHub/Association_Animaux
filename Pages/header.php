52a6693b0ca3d27a47d81502ff97d3934bcadaf1
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-04-30 19:05:35 +0200

    [edit] edit style navbar

--- Pages/header.php
+++ Pages/header.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <title>accueil</title>
 
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Accueil</a>
+  <a class="navbar-brand fa fa-home" href="accueil.php"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -18,25 +19,24 @@
       <li class="nav-item active">
         <a class="nav-link" href="accueil.php">Accueil<span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="animaux.php">Les animaux</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="animaux.php">Les animaux</span></a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="inscription.php">Inscription</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="connexion.php">Connexion</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="deconnexion.php">Déconnexion</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="panier.php">Panier</a>
-      </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="remerciement.php">Remerciements</a>
       </li>
+      <a class="nav-link fa fa-shopping-basket panier active" href="panier.php"></a>
     </ul>
+    
   
   </div>
   </nav>
