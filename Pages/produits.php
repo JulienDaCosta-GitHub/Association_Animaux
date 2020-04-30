c692585e66cec72f9377137b4ccfe3e227f8cbd8
JulienDaCosta-GitHub <55091161+JulienDaCosta-GitHub@users.noreply.github.com>
2020-04-30 23:21:57 +0200

    Nos produits
    Affiche les produits

--- Pages/produits.php
+++ Pages/produits.php
@@ -1,11 +1,55 @@
+<?php include "header.php"; ?>
+
+<?php 
+//connexion bdd
+
+  $con=mysqli_connect('localhost','root','','association_animaux', '3308');
+ 
+  if(!$con)
+  {
+      die(' Please Check Your Connection'.mysqli_error($con));
+  }
+
+  $produits =  $con->query('SELECT * FROM produit');
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Nos produits</title>
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
+
+<h2 class="title"><b>Liste des produits<b></h2>
+
+<div class="all_cards">
+
+<?php
+  foreach  ($produits as $produit) {
+      echo '<div class="card">
+      <div class="cadre"><img class="img_card" src="IMG/' . $produit["image"] . '" alt="Avatar"></div>
+      <div class="card_container">
+    
+        <h4 class="title_card"><b>' . $produit["nom"] . '</b></h4>
+        <p>Type: ' . $produit["type_animal"] . '</p>
+        <p>Prix: ' . $produit["prix"] . '€</p>
+        <p>Stock: ' . $produit["stock"] . ' unités</p>
+        <button type="button" class="btn btn-primary">Acheter</button>
+      </div>
+    </div>';
+}
+      ?>
+
+</div>
+
+
+<?php include "footer.php"; ?>
     
 </body>
 </html>
+
