bba1ac0e2e9d2f78dfd08d693db9874885a2e6ba
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-05-01 17:12:22 +0200

    Add condition's comments pages/produits

--- Pages/produits.php
+++ Pages/produits.php
@@ -33,6 +33,13 @@
   foreach  ($produits as $produit) {
       echo '<div class="card">';
 
+      /*Condition
+
+       Si l'animal est de type chien alors récupérer l'image dans le dossier corespondant miniature/Chien
+       Si l'animal est de ty pe chat alors récupérer l'imafe dans le dossier correspondant miniature/Chat
+
+       */
+
       if ($produit['type_animal']=='chien' || $produit['type_animal'] =='Chien')
       {
           echo ' <div class="cadre"><img class="img_card" src="../Admin/miniature/Chien/Produit/' . $produit["image"] . '" alt="Avatar"></div>';
