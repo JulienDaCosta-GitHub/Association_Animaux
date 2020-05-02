5ef732762b7cad4feaeb29eb9c5c042bdf3d9c2c
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-05-03 01:47:27 +0200

    Update inscription.php

--- Pages/inscription.php
+++ Pages/inscription.php
@@ -3,7 +3,7 @@
 //connexion bdd
 try{
 
-    $bdd= new PDO('mysql:host=localhost;dbname=association_animaux;charsert=utf8','root','root');
+   // $bdd= new PDO('mysql:host=localhost;dbname=association_animaux;charsert=utf8','root','root');
 
     $bdd= new PDO('mysql:host=localhost;port=3308;dbname=association_animaux;charsert=utf8','root','');
 
